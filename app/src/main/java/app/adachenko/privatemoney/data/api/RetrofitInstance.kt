a56771aae360ea5b5378dbd270787c29c7587add
Adarchenko Roman <adarchenkor@inbox.ru>
2022-09-02 07:24:23 +0300

    added retrofit and repository

--- app/src/main/java/app/adachenko/privatemoney/data/api/RetrofitInstance.kt
+++ app/src/main/java/app/adachenko/privatemoney/data/api/RetrofitInstance.kt
@@ -2,7 +2,7 @@ package app.adachenko.privatemoney.data.api
 
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
+
 
 object RetrofitInstance {
 
--- app/src/main/java/app/adachenko/privatemoney/data/repository/Repository.kt
+++ app/src/main/java/app/adachenko/privatemoney/data/repository/Repository.kt
@@ -1,4 +1,17 @@
 package app.adachenko.privatemoney.data.repository
 
+import app.adachenko.privatemoney.data.api.RetrofitInstance
+import app.adachenko.privatemoney.model.beznal.BeznalItem
+import app.adachenko.privatemoney.model.nalichka.NalichkaItem
+import retrofit2.Response
+
 class Repository {
+
+    suspend fun getnal():Response<NalichkaItem>{
+        return RetrofitInstance.api.getNalMoneys()
+    }
+
+    suspend fun getBeznal():Response<BeznalItem>{
+        return RetrofitInstance.api.getBeznalMoneys()
+    }
 }
